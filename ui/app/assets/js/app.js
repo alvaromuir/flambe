2c83eadd386704bde606d273c2108f1ff07b22d1
alvaromuir <alvaromuir@gmail.com>
2013-10-14 13:28:01 -0400

    added the backend to the repo, moved the ui to seperate folder

---
+++ ui/app/assets/js/app.js
@@ -0,0 +1,11 @@
+(function() {
+  var _ref;
+
+  this.app = (_ref = window.app) != null ? _ref : {};
+
+  define([], function() {
+    "use strict";
+    return console.log("Flambé is show 'n tell for people who love food.");
+  });
+
+}).call(this);
